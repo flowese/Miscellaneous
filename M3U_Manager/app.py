1cc083610e22f8ef8ad47af12cfe42cf2f787ead
flowese <flowese@gmail.com>
2022-07-19 12:58:12 +0200

    Update app.py

--- M3U_Manager/app.py
+++ M3U_Manager/app.py
@@ -6,79 +6,61 @@ import re # importar libreria re para buscar patrones y reemplazar.
 import os # importar libreria gestionar ficheros.
 import time # importar libreria time para gestionar tiempos.
 
-def leer_listas_m3u(): # Leer listas M3U.
-    with open('config/listas.txt', 'r') as listas_file:
-        try:        
-            listas_text = listas_file.read()
-            listas_file.close()
-            listas = eval(listas_text)
-            return listas
-        except:
-            print(' (KO) Error al leer el archivo de listas.')
-            exit()
+listas_path = 'config/listas.txt'
+canales_path = 'config/canales.txt'
+output_dir = 'output/'
 
-def descargar_m3u(url): # Descargar archivo M3U.
-    try:
-        r = requests.get(url)
-        with open('temp.m3u', 'wb') as f:
-            f.write(r.content)
-            canales = 'temp.m3u'
-            # close the file
-            f.close()
-            print(' (OK) Archivo M3U descargado.')
-    except:
-        print(' (KO) Error al descargar el archivo M3U.')
-        exit()
+def leer_listas_m3u(): # Leer listas M3U.
+    with open(listas_path, 'r') as listas_file: 
+        listas_text = listas_file.read()
+        listas_file.close()
+        listas = eval(listas_text)
+        return listas
 
 def leer_canales(): # Leer diccionario de Canales.
-    try:   
-        with open('config/canales.txt', 'r') as dict_file:
-            dict_text = dict_file.read()
-            lista_canales = eval(dict_text)
-            dict_file.close()
-            print(' (OK) Diccionario de canales cargado.')
-            return lista_canales
-    except:
-        print(' (KO) Error al leer el archivo de canales.')
-        exit()
+    with open(canales_path, 'r') as dict_file:
+        dict_text = dict_file.read()
+        lista_canales = eval(dict_text)
+        dict_file.close()
+        print(' (OK) Diccionario de canales cargado.')
+        return lista_canales
+
+def descargar_m3u(url): # Descargar archivo M3U.
+    r = requests.get(url)
+    with open('temp.m3u', 'wb') as f:
+        f.write(r.content)
+        # close the file
+        f.close()
+        print(' (OK) Archivo M3U descargado.')
 
 def generar_m3u(lista_canales, new_filename): # Procesamineto y generación archivo M3U.
-    try:
-        with open('temp.m3u', 'r') as input_file:
-            input_text = input_file.read()
-            for line in input_text.splitlines(): # buscar el canal en el diccionario
-                for canal in lista_canales: 
-                    if canal in line: # si existe el canal en la linea
-                        line = re.sub(r'tvg-logo="[^"]*"', 'tvg-logo="' + lista_canales[canal] + '"', line)# reemplazar con el logo del canal de la lista_canales
-                with open(f'output/{new_filename}.m3u', 'a') as output_file: # generamos archivo nuevo y lo cerramos
-                    output_file.write(line + '\n')
-                output_file.close()
-            input_file.close()
+    with open('temp.m3u', 'r') as f:
+        # read the file line by line
+        lines = f.readlines()
+        # loop through the lines
+        for i, line in enumerate(lines):
+            # if the line contains the tvg-name tag
+            if 'tvg-name' in line:
+                # search content for the tvg-name tag
+                tvg_name = re.search(r'tvg-name="(.*?)"', line).group(1)
+                # if the tvg-name tag is in the list of channels
+                if tvg_name in lista_canales:
+                    # replace the tvg-logo tag with the correct logo
+                    lines[i] = re.sub(r'tvg-logo="(.*?)"', r'tvg-logo="{}"'.format(lista_canales[tvg_name]), line)
+        # write the new file with the correct logos on output/
+        with open(f'{output_dir}{new_filename}.m3u', 'w') as f:
+            f.writelines(lines)
+            f.close()
             print(' (OK) Archivo M3U generado.')
-    except:
-        print(' (KO) Error al generar el archivo M3U.')
-        exit()
 
-def gestionar_temporales(): # Borrar archivos temporales
-    try:  
+if  __name__ == '__main__':
+    # 1. Leer diccionario de listas m3u.
+    diccionario_listas = leer_listas_m3u()
+    # 2. Leer diccionario de canales.
+    diccionario_canales = leer_canales()
+    # 3. Para cada usuario, descargar archivo m3u y generar archivo m3u.
+    for usuario, url_m3u in diccionario_listas.items():
+        descargar_m3u(url_m3u)
+        generar_m3u(diccionario_canales, usuario)
+        # 4. Borrar archivo temporal.
         os.remove('temp.m3u')
-        print(' (OK) Archivos temporales eliminados.')
-    except:
-        print(' (KO) Error al borrar archivos temporales.')
-        exit()
-
-
-if __name__ == '__main__': # INICIO SCRPIT
-    listas_de_usuario = leer_listas_m3u() # 1. Leer listas M3U de listas.txt.
-    for usuarios in listas_de_usuario: # 2. Descargar archivo M3U de las listas.
-        tiempo_inicio = time.time()
-        print(f'\n[{usuarios}]')
-        url = listas_de_usuario[usuarios]
-        descargar_m3u(url)
-        lista_canales = leer_canales() # 3. Leer diccionario de canales.
-        generar_m3u(lista_canales, usuarios) # 4. Procesar y generar archivo M3U.
-        gestionar_temporales() # 5. Borrar archivos temporales.
-        tiempo_fin = time.time()
-        # tiempo_total as integer
-        tiempo_total = int(tiempo_fin - tiempo_inicio)
-        print(f' (OK) Proceso finalizado en {tiempo_total} segundos.')
